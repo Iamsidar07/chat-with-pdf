49364b6b27655de42e20bff1d08fbe923048b7f4
iamsidar07 <ms8460149@gmail.com>
2024-04-08 10:00:34 +0530

    fix: susbscriptionPlan

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,13 @@
 import Dashboard from "@/components/Dashboard";
+import { getUserSubscriptionPlan } from "@/utils/stripe";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 const page = async () => {
+  const subscriptionPlan = await getUserSubscriptionPlan();
   const user = await currentUser();
   if (!user || !user.id) redirect(`/auth-callback?origin=dashboard`);
 
-  return <Dashboard userId={user?.id ?? ""} />;
+  return <Dashboard subscriptionPlan={subscriptionPlan} />;
 };
 export default page;
